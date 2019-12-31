d015de7cd270caf59cbc6c73cbbdbcc7db8a05a3
Augusto Silva <augusto_ls17@hotmail.com>
2019-12-31 00:12:53 -0300

    Ĩmplementado o cadastro de avisos e iniciado a listagem

--- src/app/add-advice/add-advice.component.ts
+++ src/app/add-advice/add-advice.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdviceService } from '../advice.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Advice } from '../advice';
 
 @Component({
   selector: 'app-add-advice',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAdviceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adviceService: AdviceService) { }
+
+  advice: Advice = new Advice();
+  submitted: boolean = false;
 
   ngOnInit() {
+    this.submitted = false;
   }
 
+  saveAdviceForm= new FormGroup ({
+    title: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(200)]),
+    description: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(200)])
+  });
+
+  saveAdvice(saveAdvice) {
+    this.advice = new Advice();
+    this.advice.title = this.adviceTitle.value;
+    this.advice.description = this.adviceDescription.value;
+    this.submitted = true;
+    this.save();
+  }
+
+  save() {
+    this.adviceService.insertAdvice(this.advice)
+        .subscribe(data => console.log(data), error => console.log(error));
+  }
+
+  get adviceTitle() {
+    return this.saveAdviceForm.get('title');
+  }
+
+  get adviceDescription() {
+    return this.saveAdviceForm.get('description');
+  }
+
+  addAdviceForm() {
+    this.submitted = false;
+    this.saveAdviceForm.reset();
+  }
 }
